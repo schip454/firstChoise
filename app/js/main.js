780a8a315535237095ad28cef5019a6ef671736f
schip <roziblanco454@gmail.com>
2023-06-13 20:45:52 +0300

    add 6 pages

--- app/js/main.js
+++ app/js/main.js
@@ -112,6 +112,23 @@ const swiperFour = new Swiper(".reviews__slider", {
   },
 });
 
+// accordion
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion__item-title"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
 const tickerItem = document.querySelectorAll(".brands__item");
 
 let pagination = document.querySelector(".brands__items");
