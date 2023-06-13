fd1a4ccf34baf1cfca8bd4f8ed59893f5c9c632b
schip <roziblanco454@gmail.com>
2023-06-13 14:11:21 +0300

    furst page

---
+++ app/js/main.js
@@ -0,0 +1,179 @@
+const swiper = new Swiper(".top__slider", {
+  // Optional parameters
+  loop: true,
+  effect: "fade",
+  pagination: {
+    el: ".top__slider-pagination",
+    type: "fraction",
+    // formatFractionCurrent: function (number) {
+    //   return "0" + number;
+    // },
+    // formatFractionTotal: function (number) {
+    //   return "0" + number;
+    // },
+
+    formatFractionCurrent: addZero,
+    formatFractionTotal: addZero,
+    renderFraction: function (currentClass, totalClass) {
+      return (
+        '<span class="' +
+        currentClass +
+        '"></span>' +
+        `
+        <svg width="34" height="1" viewBox="0 0 34 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <line y1="0.5" x2="34" y2="0.5" stroke="#202020"/>
+        </svg>
+        ` +
+        '<span class="' +
+        totalClass +
+        '"></span>'
+      );
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".top__slider-next",
+    prevEl: ".top__slider-prev",
+  },
+});
+
+function addZero(num) {
+  return num > 9 ? num : "0" + num;
+}
+
+const swiperSecond = new Swiper(".announcements__slider", {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".announcements__slider-pagination",
+  },
+});
+
+const swiperThird = new Swiper(".offers__slider", {
+  loop: true,
+  slidesPerView: 3,
+  // Optional parameters
+  navigation: {
+    nextEl: ".offers__pagination-next",
+    prevEl: ".offers__pagination-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
+
+const swiperFour = new Swiper(".reviews__slider", {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      opacity: 0,
+    },
+    next: {
+      opacity: 0,
+    },
+  },
+  pagination: {
+    el: ".reviews__slider-pagination",
+    type: "fraction",
+    // formatFractionCurrent: function (number) {
+    //   return "0" + number;
+    // },
+    // formatFractionTotal: function (number) {
+    //   return "0" + number;
+    // },
+
+    formatFractionCurrent: addZero,
+    formatFractionTotal: addZero,
+    renderFraction: function (currentClass, totalClass) {
+      return (
+        '<span class="' +
+        currentClass +
+        '"></span>' +
+        `
+        <svg width="34" height="1" viewBox="0 0 34 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <line y1="0.5" x2="34" y2="0.5" stroke="#202020"/>
+        </svg>
+        ` +
+        '<span class="' +
+        totalClass +
+        '"></span>'
+      );
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews__slider-next",
+    prevEl: ".reviews__slider-prev",
+  },
+});
+
+const tickerItem = document.querySelectorAll(".brands__item");
+
+let pagination = document.querySelector(".brands__items");
+let initialX = 0;
+let initialY = 0;
+let moveElem = false;
+
+let events = {
+  mouse: {
+    down: "mousedown",
+    move: "mousemove",
+    up: "mouseup",
+  },
+  touch: {
+    down: "touchstart",
+    move: "touchmove",
+    up: "touchend",
+  },
+};
+
+let deviceType = "";
+
+const isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (err) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+isTouchDevice();
+
+pagination.addEventListener(events[deviceType].down, (e) => {
+  e.preventDefault();
+  initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+  initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+  moveElem = true;
+});
+
+pagination.addEventListener(events[deviceType].move, (e) => {
+  if (moveElem) {
+    e.preventDefault();
+    let newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+    let newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+    pagination.style.left = pagination.offsetLeft - (initialX - newX) + "px";
+
+    initialX = newX;
+    initialY = newY;
+  }
+});
+
+pagination.addEventListener(events[deviceType].up, (e) => {
+  moveElem = false;
+});
+
+pagination.addEventListener("mouseleave", (e) => {
+  moveElem = false;
+});
+pagination.addEventListener(events[deviceType].up, (e) => {
+  moveElem = false;
+});
